c65ca08e02c44918077694080052b381f66f3089
Ivanproyectos <ivansperezt@gmail.com>
2024-06-12 12:55:45 -0500

    se agrego los archivos de configuracion de entorno

--- src/app/infraestructure/adapters/buscado.adapter.ts
+++ src/app/infraestructure/adapters/buscado.adapter.ts
@@ -3,14 +3,16 @@ import { IBuscadoRequest, IBuscadoResponse } from '../../domain/ports/buscado/bu
 import { BuscadoPort } from '../../domain/ports/buscado/buscado.port'
 import { HttpClient } from '@angular/common/http';
 import { lastValueFrom } from 'rxjs';
+import { environment } from '../../../environments/environment';
 export class BuscadoAdapter implements BuscadoPort {
+  private urlApi = `${environment.apiEndpoint}/buscados`
   private http: HttpClient = inject(HttpClient)
   obtenerBuscadoPorId(id: number): Promise<IBuscadoResponse> {
-    return lastValueFrom(this.http.get<IBuscadoResponse>(`http://localhost:3000/api/buscados/${id}`))
+    return lastValueFrom(this.http.get<IBuscadoResponse>(`${this.urlApi}/${id}`))
   }
 
   listarTodos(filtro: IBuscadoRequest): Promise<IBuscadoResponse[]> {
-    const url = `http://localhost:3000/api/buscados?categoryId=${filtro.categoriaId}&filter=${filtro.filter}`;
+    const url = `${this.urlApi}?categoryId=${filtro.categoriaId}&filter=${filtro.filter}`;
     return lastValueFrom(this.http.get<IBuscadoResponse[]>(url));
   }
 
