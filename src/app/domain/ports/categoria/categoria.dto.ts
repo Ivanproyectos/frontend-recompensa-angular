32f091a38d7799a74252b6ac868a518ec1151408
Ivanproyectos <ivansperezt@gmail.com>
2024-06-12 17:39:09 -0500

    cambio de categoria

--- src/app/domain/ports/categoria/categoria.dto.ts
+++ src/app/domain/ports/categoria/categoria.dto.ts
@@ -6,5 +6,5 @@ export const categoriaIcon: { [key: string]: string } = {
     Robo: "bx bx-run",
     Fraude: "bx bx-credit-card-alt",
     Contrabando:  "bx bx-car",
-    Homicidio:  "bx bx-ghost"
+    Acoso:  "bx bx-child"
 }
