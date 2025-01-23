77fa78738ee947ed921acd79263948bb6954bb28
Ivanproyectos <ivansperezt@gmail.com>
2025-01-22 21:35:56 -0500

    cambio de uri de la api de recompensas en host render

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiEndpoint: 'https://api-recompensa-node-production.up.railway.app/api',
+    apiEndpoint: 'https://api-recompensa-node.onrender.com/api',
   };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiEndpoint: 'https://api-recompensa-node-production.up.railway.app/api',
+    apiEndpoint: 'https://api-recompensa-node.onrender.com/api',
   };
